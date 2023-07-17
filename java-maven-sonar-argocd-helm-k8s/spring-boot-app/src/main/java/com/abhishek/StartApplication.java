ed8fa5f3a159b3a941d71076030dc36c0601457a
K5 <51rockyouk@163.com>
2023-07-17 17:41:07 +0800

    t

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven-666hhh11123664ttt");
+        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven-666hhh11123664tttSEP");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
         return "index";
     }
