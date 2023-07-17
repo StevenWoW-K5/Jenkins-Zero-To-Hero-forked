2c48b912c42cd0ae889cb21d4a71481bfbfbc1da
K5 <51rockyouk@163.com>
2023-07-17 16:32:17 +0800

    t

---
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/test/java/com/abhishek/StartApplicationTest.java
@@ -0,0 +1,34 @@
+package com.abhishek;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartApplicationTest {
+
+    @Test
+    void index() {
+
+        // 创建 StartApplication 实例
+        StartApplication startApplication = new StartApplication();
+
+        // 创建一个模拟的 Model 对象
+        Model model = Mockito.mock(Model.class);
+
+        // 调用 index 方法
+        String result = startApplication.index(model);
+
+        // 验证 model 的属性是否正确设置
+        Mockito.verify(model).addAttribute("title", "I have successfuly built a sprint boot application using Maven-666hhh11123664ttt");
+        Mockito.verify(model).addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+
+        // 验证 index 方法的返回值是否符合预期
+        assertEquals("index", result);
+    }
+
+    @Test
+    void main() {
+    }
+}
